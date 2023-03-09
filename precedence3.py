4367226c812ff658b9a7ea49ec0b1cf498d5573a
Prashanth1894 <126759450+Prashanth1894@users.noreply.github.com>
2023-03-09 10:57:36 +0530

    Add files via upload

---
+++ precedence3.py
@@ -0,0 +1,9 @@
+
+#* multiplication and floor division have the same precedence*
+# Left-right associativity
+# Output: 3
+print(5 * 2 // 3)
+
+# Shows left-right associativity
+# Output: 0
+print(5 * (2 // 3))
---
+++ precedence4.py
@@ -0,0 +1,7 @@
+# Shows the right-left associativity of **
+# Output: 512, Since 2**(3**2) = 2**9
+print(2 ** 3 ** 2)
+
+# If 2 needs to be exponated fisrt, need to use ()
+# Output: 64
+print((2 ** 3) ** 2)
